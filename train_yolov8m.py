805ead2227fe7978ef4f1e8454629099f28ab613
annesbl <annesoballa@icloud.com>
2025-06-17 01:47:38 +0200

    needed to change some stuff

--- train_yolov8m.py
+++ train_yolov8m.py
@@ -1,12 +1,9 @@
-""""
-YOLO-v8m auf dataset_full trainieren und stärkste gewichte in
-yolov8m_sneakers.pt speichern.
-"""
 import os
 from ultralytics import YOLO
 
+# Achtung: Basismodell muss YOLOv8m-SEGM sein
 DATASET_YAML = "dataset_full/dataset.yaml"     
-BASE_MODEL   = "yolov8m.pt"
+BASE_MODEL   = "yolov8m-seg.pt"  # <- das ist das Segmentation-Model!
 OUT_WEIGHTS  = "yolov8m_sneakers.pt"
 
 def main():
@@ -21,16 +18,9 @@ def main():
         device   = 0            #GPU 0
     )
 
-    best = model.trainer.best   #Pfad zu best.pt
+    best = model.trainer.best   # Pfad zu best.pt
     os.rename(best, OUT_WEIGHTS)
     print(f"[✓] Training done – weights saved as {OUT_WEIGHTS}")
 
 if __name__ == "__main__":
     main()
-
-
-#für rechner 
-"""
-pip install ultralytics==0.6.6   opencv-python  numpy
-python train_yolov8m.py
-"""
